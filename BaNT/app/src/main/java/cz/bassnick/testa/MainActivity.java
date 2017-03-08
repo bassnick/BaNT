dc9548f040e1de4c7c40ad19f7a8a301e360628d
bassnick <bassnick@seznam.cz>
2017-03-08 21:45:31 +0100

    Uspesne stazeni testovacich dat z munzee API

--- BaNT/app/src/main/java/cz/bassnick/testa/MainActivity.java
+++ BaNT/app/src/main/java/cz/bassnick/testa/MainActivity.java
@@ -1,6 +1,8 @@
 package cz.bassnick.testa;
 
 import android.location.Address;
+import android.net.http.HttpResponseCache;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,22 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -64,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             a.getAddressLine(2);
             textview = (TextView) findViewById(R.id.tvCountry);
             textview.setText(a.getAddressLine(2));
-        }catch (Exception exc){  }
+        }catch (Exception exc){     }
         //   }
 
         //else
@@ -82,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         17.97093591094017
         *
         */
+        //KiJvZ85biNSZ7R8bR6BkbSdyvtiWfYv8pHL2HX3g
     }
 
     @Override
@@ -105,6 +124,42 @@ public class MainActivity extends AppCompatActivity {
         ((TextView) (findViewById(R.id.tvCountMunzee))).setText("xxx");
         ((TextView) (findViewById(R.id.tvCountSpecial))).setText("yyy");
         MakeGPSView();
+        PostTask p = new PostTask();
+        AsyncTask<String, String, String> result = p.execute(null,null,null);
 
+
+    }
+    private class PostTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... data) {
+            try {
+                //add data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                String token = "KiJvZ85biNSZ7R8bR6BkbSdyvtiWfYv8pHL2HX3g";
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("https://api.munzee.com/user/");
+                httppost.setHeader("Authorization", "Bearer " + token);
+                nameValuePairs.add(new BasicNameValuePair("data", "{\"username\":\"coolant\"}"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                //execute http post
+                HttpResponse response = httpclient.execute(httppost);
+                String result = new BasicResponseHandler().handleResponse(response);
+                return result;
+            } catch (ClientProtocolException e) {
+                return e.getMessage();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+             catch (Throwable t)
+            {
+                return t.getMessage();
+            }
+        }
+        @Override
+        protected void onPostExecute(String result)
+        {
+            // todo
+            ((TextView) (findViewById(R.id.tvCountMunzee))).setText(result.substring(0,20));
+        }
     }
 }
